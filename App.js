94ffe7d3b0d4022281e940bf763441a15cc14b98
Alex Luu <103136100+LuuAlex@users.noreply.github.com>
2023-05-26 10:35:29 -0700

    work on initial screen

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.h1}>Encrypted Local Password Manager</Text>
+      <Text style={styles.p}>Please make a selection below:</Text>
+      <Button 
+        style={styles.button}
+        onPress={null} 
+        title='I need a new password database (new users)' />
+      <Button 
+        style={styles.button}
+        onPress={null} 
+        title='I already have an existing password database' />
       <StatusBar style="auto" />
     </View>
   );
 }
 
+var {height, width} = Dimensions.get('window');
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#da635d',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  h1: {
+    marginTop: height * 0.1,
+    marginLeft: width * 0.02,
+    marginRight: width * 0.02,
+    fontFamily: 'Helvetica',
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 40,
+    textAlign: 'center',
+  },
+  p: {
+    marginTop: height * 0.05,
+    marginLeft: width * 0.05,
+    marginRight: width * 0.05,
+    fontFamily: 'Helvetica',
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 25,
+    textAlign: 'center',
+  },
+  button: {
+    marginTop: height * 0.2,
+    marginLeft: width * 0.05,
+    marginRight: width * 0.05,
+    fontFamily: 'Helvetica',
+    color: '#fff',
+    backgroundColor: '#fff',
+    fontWeight: 'bold',
+    fontSize: 25,
+    textAlign: 'center',
   },
 });
+
+// Colors: https://www.color-hex.com/color-palette/3307
