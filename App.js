354092b4d1157605ba08f1c11a879291318e5613
Alex Luu <103136100+LuuAlex@users.noreply.github.com>
2023-05-26 21:12:10 -0700

    finished opening screen ui

--- App.js
+++ App.js
@@ -1,61 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Dimensions, Button } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.h1}>Encrypted Local Password Manager</Text>
       <Text style={styles.p}>Please make a selection below:</Text>
-      <Button 
-        style={styles.button}
-        onPress={null} 
-        title='I need a new password database (new users)' />
-      <Button 
-        style={styles.button}
-        onPress={null} 
-        title='I already have an existing password database' />
+      <View style={styles.buttonGroup}>
+        <View style={styles.button}>
+          <TouchableOpacity
+            style={styles.buttonButton}
+            onPress={null}
+          >
+            <Text style={styles.p}>I need a new password database</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.button}>
+          <TouchableOpacity
+            style={styles.buttonButton}
+            onPress={null}
+          >
+            <Text style={styles.p}>I already have an existing password database</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-var {height, width} = Dimensions.get('window');
+var { height, width } = Dimensions.get("window");
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#da635d',
-    alignItems: 'center',
+    backgroundColor: "#da635d",
+    alignItems: "center",
   },
   h1: {
-    marginTop: height * 0.1,
-    marginLeft: width * 0.02,
-    marginRight: width * 0.02,
-    fontFamily: 'Helvetica',
-    color: '#fff',
-    fontWeight: 'bold',
+    marginTop: height * 0.16,
+    marginHorizontal: width * 0.02,
+    fontFamily: "Helvetica",
+    color: "#fff",
+    fontWeight: "bold",
     fontSize: 40,
-    textAlign: 'center',
+    textAlign: "center",
   },
   p: {
-    marginTop: height * 0.05,
-    marginLeft: width * 0.05,
-    marginRight: width * 0.05,
-    fontFamily: 'Helvetica',
-    color: '#fff',
-    fontWeight: 'bold',
+    marginVertical: height * 0.025,
+    marginHorizontal: width * 0.05,
+    fontFamily: "Helvetica",
+    color: "#fff",
+    fontWeight: "bold",
     fontSize: 25,
-    textAlign: 'center',
+    textAlign: "center",
+  },
+  buttonGroup: {
+    marginTop: height * 0.05,
   },
   button: {
-    marginTop: height * 0.2,
-    marginLeft: width * 0.05,
-    marginRight: width * 0.05,
-    fontFamily: 'Helvetica',
-    color: '#fff',
-    backgroundColor: '#fff',
-    fontWeight: 'bold',
-    fontSize: 25,
-    textAlign: 'center',
+    marginVertical: height * 0.02,
+    marginHorizontal: width * 0.05,
+  },
+  buttonButton: {
+    borderWidth: 8,
+    borderStyle: 'solid',
+    borderRadius: 25,
+    borderColor: '#fff',
+    backgroundColor: '#4e4e56'
   },
 });
 
