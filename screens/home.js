2279c6cac0acfaa15de60eb7083376b3bfe56569
Alex Luu <103136100+LuuAlex@users.noreply.github.com>
2023-07-14 18:23:24 -0700

    changed home screen to hide passwords and only show on click

--- screens/home.js
+++ screens/home.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import Encryption from '../encryption_tools/encryption.js'
+import Encryption from '../encryption_tools/encryption.js';
 import {
   StyleSheet,
   Text,
   View,
   Dimensions,
   TouchableOpacity,
+  TouchableWithoutFeedback,
   TextInput,
   ScrollView,
   StatusBar,
-  UseState,
   RefreshControl,
+  Modal,
 } from 'react-native';
 var RNFS = require('react-native-fs');
 
-export default function Home({ route, navigation }) {
-  const { password } = route.params;
+export default function Home({route, navigation}) {
+  const {password} = route.params;
 
   const [refreshing, setRefreshing] = React.useState(false);
   const [data, setData] = React.useState(null);
   const dataLoc = `${RNFS.DocumentDirectoryPath}/LocalPasswordStorageDATA`;
   readFile();
+
+  const [modalVisible, setModalVisible] = React.useState(false);
+  const [d, setD] = React.useState(false);
 
   async function readFile() {
     const fileUri1 = `${dataLoc}/user.txt`;
@@ -31,6 +35,11 @@ export default function Home({ route, navigation }) {
     var dContent = Encryption.decrypt(content, hashInfo);
     setData(dContent);
     return dContent;
+  }
+
+  function modal(d) {
+    setModalVisible(true);
+    setD(d);
   }
 
   const onRefresh = React.useCallback(() => {
@@ -44,7 +53,7 @@ export default function Home({ route, navigation }) {
     <ScrollView
       automaticallyAdjustKeyboardInsets={true}
       contentContainerStyle={styles.container}
-      style={{backgroundColor:'#da635d'}}
+      style={{backgroundColor: '#da635d'}}
       refreshControl={
         <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
       }>
@@ -54,28 +63,54 @@ export default function Home({ route, navigation }) {
         <Text style={styles.nav}>&#xFF0B; New</Text>
       </TouchableOpacity>
       <Text style={styles.h1}>Saved Credentials</Text>
-      <Entry data={data} />
-      <View style={{marginVertical:20}}></View>
+      <Text style={styles.p}>Click an account to see details.</Text>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          console.log('Modal has been closed.');
+          setModalVisible(!modalVisible);
+        }}>
+        <View style={styles.modalStyle}>
+          <TouchableOpacity
+            style={styles.close}
+            onPress={() => setModalVisible(false)}>
+            <Text style={styles.mnav}>&#x2715; Close</Text>
+          </TouchableOpacity>
+          <View style={styles.ebox}>
+            <Text style={styles.boxEP1}>{d[0]}</Text>
+            <Text style={styles.boxEP2}>Username: {d[1]}</Text>
+            <Text style={styles.boxEP2}>Password: {d[2]}</Text>
+          </View>
+          <View style={styles.h1}></View>
+          <View style={styles.p}></View>
+        </View>
+      </Modal>
+      <View style={styles.p}></View>
+      <View style={styles.p}></View>
+      <Entry data={data} modal={x => modal(x)} />
+      <View style={{marginVertical: 20}}></View>
       <StatusBar style="auto" />
     </ScrollView>
   );
 }
 
-export function Entry(data) {
-  data = data.data;
+export function Entry({data, modal}) {
   const dataAry = String(data).split('\n\n').slice(0, -1);
   const rv = dataAry.map(d => (
-    <View key={d} style={styles.box}>
+    <TouchableOpacity
+      key={d}
+      style={styles.box}
+      onPress={() => modal(d.split('\n'))}>
       <Text style={styles.boxP1}>{d.split('\n')[0]}</Text>
+      {/* 
       <Text style={styles.boxP2}>Username: {d.split('\n')[1]}</Text>
       <Text style={styles.boxP2}>Password: {d.split('\n')[2]}</Text>
-    </View>
+      */}
+    </TouchableOpacity>
   ));
   return rv;
-}
-
-async function getData(fileUri) {
-  return await RNFS.readFile(fileUri);
 }
 
 var {height, width} = Dimensions.get('window');
@@ -136,6 +171,7 @@ const styles = StyleSheet.create({
   box: {
     marginVertical: height * 0.01,
     marginHorizontal: width * 0.01,
+    paddingVertical: height * 0.006,
     borderWidth: 3,
     borderStyle: 'solid',
     borderRadius: 5,
@@ -161,6 +197,57 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 16,
     textAlign: 'left',
+  },
+  modalStyle: {
+    flexGrow: 1,
+    flexDirection: 'column',
+    backgroundColor: '#da635d',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  ebox: {
+    flexDirection: 'column',
+    marginHorizontal: width * 0.01,
+    paddingVertical: height * 0.015,
+    width: width * 0.8,
+    height: height * 0.4,
+    borderWidth: 3,
+    borderStyle: 'solid',
+    borderRadius: 5,
+    borderColor: '#fff',
+    backgroundColor: '#4e4e56',
+  },
+  boxEP1: {
+    marginVertical: height * 0.015,
+    marginHorizontal: width * 0.05,
+    width: width * 0.65,
+    fontFamily: 'Helvetica',
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 25,
+    textAlign: 'center',
+  },
+  boxEP2: {
+    marginVertical: height * 0.01,
+    marginHorizontal: width * 0.05,
+    width: width * 0.65,
+    fontFamily: 'Helvetica',
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 23,
+    textAlign: 'left',
+  },
+  close: {
+    marginBottom: height * 0.02,
+    marginHorizontal: width * 0.02,
+    textAlign: 'center',
+  },
+  mnav: {
+    fontFamily: 'Helvetica',
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 25,
+    alignItems: 'center',
   },
 });
 
